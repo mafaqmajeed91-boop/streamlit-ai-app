10c4af8bf84d030809149f1795d51813cb757e66
mafaqmajeed91-boop <mafaqmajeed91@gmail.com>
2026-06-04 14:47:21 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import numpy as np
 import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
- import matplotlib.pyplot as plt
 git add requirements.txt
 git commit -m "added matplotlib dependency"
 git push
