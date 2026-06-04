061b50dc89cc416070dbccd326f12e818bd2c22f
mafaqmajeed91-boop <mafaqmajeed91@gmail.com>
2026-06-04 14:45:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
+ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # --- Load Models ---
